4377ce4cbd29f5127cb4fdfe0ca1714efe88e144
GawnGyu Yeo <bbc02131@naver.com>
2022-12-14 15:04:18 +0900

    string

---
+++ javascript-peactices/basics/ex14.js
@@ -0,0 +1,47 @@
+/*
+URL 다루기
+*/
+var url= 'http://www.mysite.com/user?name=둘리&no=10&email=bbc02131@naver.com';
+
+//1. escape : URL 전부를 encoding, 사용(x), deprecated
+var url2 = escape(url);
+console.log(url2);
+
+//2.encodeURI : URL 중 파라미터만 encoding하기 때문에 URL전체를 encoding 해야하는 경우, 사용(o)
+var url3 = encodeURI(url);
+console.log(url3);
+
+//3.encodeURIComponent: 값만 Encoding해야 하는 경우, 사용(o)
+//잘못사용하는 경우 
+var url4 = encodeURIComponent(url);
+console.log(url4);
+
+//4.
+//만들어야헐 URL
+//http://www.mysite.com/user?name=둘리&no=10&email=bbc02131@naver.com
+var url='http://www.mysite.com/user';
+var formData={
+    no:10,
+    name: '둘리',
+    email: 'bbc02131@naver.com'
+}
+
+var toQueryString=function(o){
+    var qs=[];
+    for(prop in o){
+        qs.push(prop+"="+encodeURIComponent(o[prop]));
+    }
+
+    return qs.join("&");
+}
+//사용 예1
+var url5=url +"?"+toQueryString(formData);
+console.log(url5);
+
+
+//사용예2 :jquery
+// jQuery.ajax({
+//     url:"",
+//     type:"post",
+//     data: toQueryString(formData)
+// });
